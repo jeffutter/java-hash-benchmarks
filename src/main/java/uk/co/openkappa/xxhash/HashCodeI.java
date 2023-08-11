b20ef6cc7c557ddc2281d8b633d69b14d584dd5a
Jeffery Utter <jeffery.utter@thescore.com>
2023-08-11 11:35:38 -0500

    WIP ByteBuffer

---
+++ src/main/java/uk/co/openkappa/xxhash/HashCodeI.java
@@ -0,0 +1,8 @@
+package uk.co.openkappa.xxhash;
+
+import java.nio.ByteBuffer;
+
+@FunctionalInterface
+public interface HashCodeI {
+    int hash(ByteBuffer data, int seed);
+}
--- src/main/java/uk/co/openkappa/xxhash/benchmarks/HashCode.java
+++ src/main/java/uk/co/openkappa/xxhash/benchmarks/HashCode.java
@@ -11,6 +11,9 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 
+import java.lang.foreign.MemorySegment;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -19,35 +22,42 @@ public class HashCode {
     @Param({"10", "100", "500", "1000", "2000"})
     int size;
 
-    @Param({"VECTOR", "HASH_CODE", "SCALAR"})
+    @Param({"VECTOR", "VECTOR_COPY", "HASH_CODE", "SCALAR"})
     Impl impl;
 
-    byte[] data;
+//    byte[] data;
+    ByteBuffer data;
 
-    Hasher32 hasher;
+    HashCodeI hasher;
 
     public enum Impl {
         HASH_CODE {
             @Override
-            Hasher32 create() {
+            HashCodeI create() {
                 return new HashCodeHasher();
             }
         },
 
         SCALAR {
             @Override
-            Hasher32 create() {
+            HashCodeI create() {
                 return new ScalarHasher();
             }
         },
 
         VECTOR {
             @Override
-            Hasher32 create() {
+            HashCodeI create() {
                 return new SIMDHasher();
             }
+        },
+        VECTOR_COPY {
+            @Override
+            HashCodeI create() {
+                return new SIMDHasherCopy();
+            }
         };
-        abstract Hasher32 create();
+        abstract HashCodeI create();
     }
 
 
@@ -91,23 +101,25 @@ public class HashCode {
         H_COEFF_32 = IntVector.fromArray(INT_256_SPECIES, x, 0);
     }
 
-    static class HashCodeHasher implements Hasher32 {
+    static class HashCodeHasher implements HashCodeI {
         @Override
-        public int hash(byte[] data, int _seed) {
+        public int hash(ByteBuffer data, int _seed) {
             return data.hashCode();
         }
     }
 
-    static class ScalarHasher implements Hasher32 {
+    static class ScalarHasher implements  HashCodeI {
         @Override
-        public int hash(byte[] data, int _seed) {
-            return Arrays.hashCode(data);
+        public int hash(ByteBuffer data, int _seed) {
+            return Arrays.hashCode(data.array());
         }
     }
 
-    static class SIMDHasher implements Hasher32 {
+    static class SIMDHasherCopy implements HashCodeI {
         @Override
-        public int hash(byte[] data, int _seed) {
+        public int hash(ByteBuffer bbdata, int _seed) {
+            var data = bbdata.array();
+
             IntVector h1 = IntVector.fromArray(INT_256_SPECIES, new int[]{1, 0, 0, 0, 0, 0, 0, 0}, 0);
             IntVector h2 = IntVector.zero(INT_256_SPECIES);
             IntVector h3 = IntVector.zero(INT_256_SPECIES);
@@ -142,12 +154,78 @@ public class HashCode {
         }
     }
 
+    static class SIMDHasher implements HashCodeI {
+        @Override
+        public int hash(ByteBuffer data, int _seed) {
+            var origPos = data.position();
+            var ms = MemorySegment.ofBuffer(data);
+
+            IntVector h1 = IntVector.fromArray(INT_256_SPECIES, new int[]{1, 0, 0, 0, 0, 0, 0, 0}, 0);
+            IntVector h2 = IntVector.zero(INT_256_SPECIES);
+            IntVector h3 = IntVector.zero(INT_256_SPECIES);
+            IntVector h4 = IntVector.zero(INT_256_SPECIES);
+            int i = 0;
+//            for (; i < (data.length & ~(BYTE_256_SPECIES.length() - 1)); i += BYTE_256_SPECIES.length()) {
+            var initLen = data.remaining();
+            var bound = initLen & ~(BYTE_256_SPECIES.length() - 1);
+            while(i < (initLen & ~(BYTE_256_SPECIES.length() - 1))) {
+//                ByteVector b = ByteVector.fromArray(BYTE_64_SPECIES, data, i);
+                ByteVector b = ByteVector.fromMemorySegment(BYTE_64_SPECIES, ms, i, ByteOrder.nativeOrder());
+//                data.position(data.position() + BYTE_64_SPECIES.length());
+
+                IntVector x = (IntVector) b.castShape(INT_256_SPECIES, 0);
+                h1 = h1.mul(H_COEFF_31_TO_32).add(x.mul(H_COEFF_32));
+
+//                b = ByteVector.fromArray(BYTE_64_SPECIES, data, i + BYTE_64_SPECIES.length());
+//                b = ByteVector.fromMemorySegment(BYTE_64_SPECIES, MemorySegment.ofBuffer(data.slice(0, BYTE_64_SPECIES.length())), 0, ByteOrder.nativeOrder());
+//                data.position(data.position() + BYTE_64_SPECIES.length());
+                b = ByteVector.fromMemorySegment(BYTE_64_SPECIES, ms, i + BYTE_64_SPECIES.length(), ByteOrder.nativeOrder());
+
+                x = (IntVector) b.castShape(INT_256_SPECIES, 0);
+                h2 = h2.mul(H_COEFF_31_TO_32).add(x.mul(H_COEFF_24));
+
+//                b = ByteVector.fromArray(BYTE_64_SPECIES, data, i + BYTE_64_SPECIES.length() * 2);
+//                b = ByteVector.fromMemorySegment(BYTE_64_SPECIES, MemorySegment.ofBuffer(data.slice(0, BYTE_64_SPECIES.length())), 0, ByteOrder.nativeOrder());
+                b = ByteVector.fromMemorySegment(BYTE_64_SPECIES, ms, i + BYTE_64_SPECIES.length() * 2L, ByteOrder.nativeOrder());
+                data.position(data.position() + BYTE_64_SPECIES.length());
+
+                x = (IntVector) b.castShape(INT_256_SPECIES, 0);
+                h3 = h3.mul(H_COEFF_31_TO_32).add(x.mul(H_COEFF_16));
+
+//                b = ByteVector.fromArray(BYTE_64_SPECIES, data, i + BYTE_64_SPECIES.length() * 3);
+//                b = ByteVector.fromMemorySegment(BYTE_64_SPECIES, MemorySegment.ofBuffer(data.slice(0, BYTE_64_SPECIES.length())), 0, ByteOrder.nativeOrder());
+                b = ByteVector.fromMemorySegment(BYTE_64_SPECIES, ms, i + BYTE_64_SPECIES.length() * 3L, ByteOrder.nativeOrder());
+//                data.position(BYTE_256_SPECIES.length() * 4);
+
+                x = (IntVector) b.castShape(INT_256_SPECIES, 0);
+                h4 = h4.mul(H_COEFF_31_TO_32).add(x.mul(H_COEFF_8));
+
+                i += BYTE_256_SPECIES.length();
+            }
+
+            int sh = h1.reduceLanes(VectorOperators.ADD) +
+                    h2.reduceLanes(VectorOperators.ADD) +
+                    h3.reduceLanes(VectorOperators.ADD) +
+                    h4.reduceLanes(VectorOperators.ADD);
+//            for (; i < data.length; i++) {
+//                sh = 31 * sh + data[i];
+//            }
+            data.position(origPos);
+            while(data.hasRemaining()) {
+               sh = 31 * sh + data.get();
+            }
+            data.position(origPos);
+            return sh;
+        }
+    }
+
 
     @Setup(Level.Trial)
     public void init() {
 //        data = BenchmarkUtils.newByteArray(size);
-        data = new byte[size];
-        ThreadLocalRandom.current().nextBytes(data);
+        var d = new byte[size];
+        ThreadLocalRandom.current().nextBytes(d);
+        data = ByteBuffer.wrap(d);
         hasher = impl.create();
     }
 }
